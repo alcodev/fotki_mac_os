5ca6e09f32add89d51f5b3fe3d5e400de2eb9263
Vladimir Kuznetsov <vavaka@gmail.com>
2011-12-24 11:47:48 +0600

    Initial Commit

---
+++ FotkiTests/FotkiTests.h
@@ -0,0 +1,13 @@
+//
+//  FotkiTests.h
+//  FotkiTests
+//
+//  Created by Vladimir Kuznetsov on 12/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FotkiTests : SenTestCase
+
+@end
---
+++ FotkiTests/FotkiTests.m
@@ -0,0 +1,32 @@
+//
+//  FotkiTests.m
+//  FotkiTests
+//
+//  Created by Vladimir Kuznetsov on 12/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FotkiTests.h"
+
+@implementation FotkiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FotkiTests");
+}
+
+@end
